44a7c88c2f76d01ffa08045421f4db2815ebd35d
Souad001 <114157934+Souad001@users.noreply.github.com>
2022-09-22 11:13:26 -0400

    Create poeme-paix.py

---
+++ poeme-paix.py
@@ -0,0 +1,11 @@
+# SI TU VEUX LA PAIX, PREPARE…
+
+
+#        … Surtout, ne prépare pas la guerre.
+
+
+# Bien au contraire, si tu veux la paix, prépare d´abord la fraternité.
+
+
+# Mais aussi, si tu veux la paix, prépare l´enseignement de l´amour du prochain.
+# Car, en effet, si tu veux la paix, donne la priorité des priorités à l´éducation.
